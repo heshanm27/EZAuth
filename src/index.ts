ed74c69142031da4534de88307c028ea44508e07
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-04 14:41:33 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { handleSignIn, validate } from "./function/signin";
-export { handleSignUp, validate as SignupValidate } from "./function/signup";
+export { handleSignIn } from "./function/signin";
+export { handleSignUp } from "./function/signup";
